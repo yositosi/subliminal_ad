a27cd04c809dcc90e4c045605ac5b6bf2ec232bb
Toshiaki Yoshida <yositosi@gmail.com>
2013-02-17 19:59:20 +0900

    add new

--- bookmarklet.js
+++ bookmarklet.js
@@ -8,50 +8,52 @@ var debug = false;
 		.js(debug ? "http://subliminal.jp/subliminal_ad.js" : "https://raw.github.com/yositosi/subliminal_ad/master/subliminal_ad.js", function(){
 			
 			$(document).ready(function(){
-				/*
-				new SubliminalAd({
-					img: "http://www.shopatrip.com/wp-content/uploads/2012/10/serina.jpeg",
-					interval: 3000
-				});
-				*/
-				new SubliminalAd2({
-					imgs:[
-                    /*
-				     "http://www.shopatrip.com/wp-content/uploads/2012/10/serina.jpeg",
-				     "http://laplatavalley.net/image/1349.jpg",
-				     "http://blog-imgs-43.fc2.com/y/u/y/yuyu947/img_328669_3932424_0.jpg",
-				     "http://iphonewp.net/wp-content/uploads/2011/05/oshimayuko_a10.jpg",
-                     */
-                     /* 1 */
-                     /* 2 */
-                     "http://i.yimg.jp/images/shp_blog/trend/images/2012/bakusoku/200_240_IMG_6166.jpg",
-                     /* 3 */
-                     /*
-                     "http://25.media.tumblr.com/tumblr_megfpwr5i11qeei56o1_500.jpg",
-                     "http://i.yimg.jp/images/shp_edit/cms/sale/whatshot/mens/00019/IMG_5719.jpg",
-                     "http://i.yimg.jp/images/shp_edit/cms/sale/whatshot/mens/00019/IMG_6048.jpg",
-                     "http://i.yimg.jp/images/shp_blog/trend/images/2012/bakusoku/146_orange.jpg",
-                     "http://i.yimg.jp/images/shp_blog/report/images/2013/02/05_img4.jpg",
-                     */
-                     "http://smslab.jp/wp-content/uploads/2012/08/2e4472419f8ffde8d420c9a446945a15-180x180.jpeg",
-                     /* 4 */
-                     /* 5 */
-                     "http://i.yimg.jp/images/hackday/img/partner/idcf.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/jaxa.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/olympus.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/kait.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/klab.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/gree.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/kddi_webcomm.png",
-                     "http://i.yimg.jp/images/hackday/img/partner/cyberagent.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/daiwahouse.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/nifty.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/nec.jpg",
-                     "http://i.yimg.jp/images/hackday/img/partner/microsoft.png",
-                     "http://i.yimg.jp/images/hackday/img/partner/rakuten.jpg",
-                     "http://yhacks.jp/wordpress/wp-content/uploads/2013/01/Yahoo_JAPAN.jpg",
-					]
-				});
+				if(true){
+					new SubliminalAd({
+						img: "http://www.shopatrip.com/wp-content/uploads/2012/10/serina.jpeg",
+						interval: 3000
+					});
+				}else{
+					new SubliminalAd2({
+						imgs:[
+	                    /*
+					     "http://www.shopatrip.com/wp-content/uploads/2012/10/serina.jpeg",
+					     "http://laplatavalley.net/image/1349.jpg",
+					     "http://blog-imgs-43.fc2.com/y/u/y/yuyu947/img_328669_3932424_0.jpg",
+					     "http://iphonewp.net/wp-content/uploads/2011/05/oshimayuko_a10.jpg",
+	                     */
+	                     /* 1 */
+	                     /* 2 */
+	                     "http://i.yimg.jp/images/shp_blog/trend/images/2012/bakusoku/200_240_IMG_6166.jpg",
+	                     /* 3 */
+	                     /*
+	                     "http://25.media.tumblr.com/tumblr_megfpwr5i11qeei56o1_500.jpg",
+	                     "http://i.yimg.jp/images/shp_edit/cms/sale/whatshot/mens/00019/IMG_5719.jpg",
+	                     "http://i.yimg.jp/images/shp_edit/cms/sale/whatshot/mens/00019/IMG_6048.jpg",
+	                     "http://i.yimg.jp/images/shp_blog/trend/images/2012/bakusoku/146_orange.jpg",
+	                     "http://i.yimg.jp/images/shp_blog/report/images/2013/02/05_img4.jpg",
+	                     */
+	                     "http://smslab.jp/wp-content/uploads/2012/08/2e4472419f8ffde8d420c9a446945a15-180x180.jpeg",
+	                     /* 4 */
+	                     /* 5 */
+	                     "http://i.yimg.jp/images/hackday/img/partner/idcf.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/jaxa.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/olympus.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/kait.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/klab.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/gree.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/kddi_webcomm.png",
+	                     "http://i.yimg.jp/images/hackday/img/partner/cyberagent.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/daiwahouse.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/nifty.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/nec.jpg",
+	                     "http://i.yimg.jp/images/hackday/img/partner/microsoft.png",
+	                     "http://i.yimg.jp/images/hackday/img/partner/rakuten.jpg",
+	                     "http://yhacks.jp/wordpress/wp-content/uploads/2013/01/Yahoo_JAPAN.jpg",
+						]
+					});
+					
+				}
 			});
 			
 		});
